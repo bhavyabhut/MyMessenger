d99f346a031ecf69787db632437f050b19347d8a
Bhavya Bhut <pbhavy4@gmail.com>
2021-04-13 20:48:52 +0530

    add on and emit events

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,80 @@
 import logo from "./logo.svg";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:5000", {
+  transports: ["websocket", "polling"],
+});
 
 function App() {
+  const [msg, setMsg] = useState();
+  const [id, setId] = useState();
+  const [myData, setMyData] = useState();
+  const [allUsers, setAllUsers] = useState();
+
+  // useEffect(() => {
+  //   getData();
+  // }, [videoRef]);
+
+  useEffect(() => {
+    socket.on("getData", (data) => console.log(data));
+    socket.on("login_error", (data) => console.log(data));
+    socket.on("new_user", (data) => setAllUsers(data));
+    socket.on("login_success", (data) => {
+      setMyData(data.myData);
+      setAllUsers(data.allUser);
+    });
+  }, []);
+  const submit = () => {
+    socket.emit("login", { name: id });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {myData ? (
+        <>
+          <h2>Welcome {myData.name}</h2>
+
+          {allUsers &&
+            allUsers.length > 0 &&
+            allUsers
+              .filter((d) => d.id !== myData.id)
+              .map((user) => {
+                return (
+                  <>
+                    <li>{user.name}</li>
+                  </>
+                );
+              })}
+        </>
+      ) : (
+        <>
+          <input
+            value={id}
+            type="text"
+            onChange={(e) => {
+              setId(e.target.value);
+            }}
+          ></input>
+          <input
+            value={msg}
+            type="text"
+            onChange={(e) => {
+              setMsg(e.target.value);
+            }}
+          ></input>
+
+          <button onClick={submit}> Submit</button>
+        </>
+      )}
+    </>
   );
 }
 
 export default App;
+
+const a = "name";
+
+const person = {
+  [a]: "sdfsdf",
+};
--- index.js
+++ index.js
@@ -1,13 +1,68 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 const server = http.createServer(app);
-const io = require("socket.io")(server);
-io.on("connection", (s) => {
-  console.log(s);
+const io = require("socket.io")(server, {
+  transports: ["websocket", "polling"],
 });
+
+let users = [];
+
+const socketBasicEmitMethod = (socket, method, data) =>
+  socket.emit(method, data);
+
+const socketBasicBroadcastMethod = (socket, method, data) =>
+  socket.broadcast.emit(method, data);
+
+io.on("connection", (socket) => {
+  console.log("client connected", socket.id);
+
+  socket.on("login", (data) => {
+    if (Object.keys(data).length === 0) {
+      socketBasicEmitMethod(socket, "login_error", "Please enter name");
+      return;
+    }
+    if (!data.name) {
+      socketBasicEmitMethod(socket, "login_error", "Please enter name");
+      return;
+    }
+
+    const matchUser = users.filter((user) => user.name === data.name);
+
+    if (matchUser.length > 0) {
+      socketBasicEmitMethod(
+        socket,
+        "login_error",
+        "Name is already in use! Please enter a new name"
+      );
+      return;
+    }
+    users.push({ ...data, id: socket.id });
+    socketBasicEmitMethod(socket, "login_success", {
+      myData: { ...data, id: socket.id },
+      allUser: users,
+    });
+    socketBasicBroadcastMethod(socket, "new_user", users);
+  });
+
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.broadcast.to(data.id).emit("getData", data);
+  });
+
+  socket.on("disconnect", (data) => {
+    console.log(data, socket.id);
+    let newUser = users.filter((user) => user.id !== socket.id);
+    users = [...newUser];
+    socketBasicBroadcastMethod(socket, "new_user", users);
+  });
+});
+
 server.listen("5000", () => {
-  console.log("server is running on 5000");
+  console.log("server is running o 5000");
 });
